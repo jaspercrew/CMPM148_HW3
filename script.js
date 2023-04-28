317d4ffc07ae873a95c92ba35978e52ff8f2e7ea
jaspercrew <47955720+jaspercrew@users.noreply.github.com>
2023-04-27 19:14:03 -0700

    more grammar

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function generate_prompt(){
         "origin":["#intro#"],
         "intro":["You #chefs# have been tasked with #making# a culinary masterpiece, linking together ten ingredients to make a unique dish. Today, you will be making a(n) #adj_good# and #modifier# "],
         "chefs":["chefs", "cooks"],
-        "adj_good":["delicious"," tasty", "exquisite", "mouth-watering"],
+        "adj_good":["delicious"," tasty", "exquisite", "mouth-watering", "delectable", "appetizing"],
         "making":["making", "creating", "cooking", "cooking up", "crafting"],
-        "modifier":["sweet", "salty", "sour", "spicy", "savory", "tangy", "healthy", "extremely unhealthy"],
+        "modifier":["sweet", "salty", "sour", "spicy", "savory", "tangy", "healthy", "extremely unhealthy", "chewy"],
         "dish":["pizza", "ice cream", "frittata", "cake", "pie", "sandwich", "burger", "fried rice", "soup", "stew", "smoothie"]
     }
     g = tracery.createGrammar(data)
@@ -31,11 +31,14 @@ function generate_prompt(){
 
   function generate_end(){
     data = {
-        "origin":["#intro# #main# \"#final#\""],
+        "origin":["#intro# #main# #final# #extra# #bon#"],
         "intro":["You have been completed your #adj_good# dish.", "Your #adj_good# masterpiece is ready.", "You are done with the #adj_good# dish."],
-        "adj_good":["delicious"," tasty", "exquisite"],
+        "adj_good":["delicious"," tasty", "exquisite", "delectable", "tempting", "appetizing", "yummy", "finger licking", "flavorful"],
         "main":["Your " + dish_str + " is made with  " + ing_list],
-        "final":["Enjoy.", "Bon appetite.", "Good eating!"]
+        "final":["This dish will change the world.", "This dish will never be forgotten.", "This creation will be remembered forever."],
+        "extra":["Awards from all over will be sent for this dish.", "Michelin Stars will definitely be designated for this dish.", "This dish will be the talk of the town for years."],
+        "bon":["Bon appetite!", "Enjoy!", "Good eating!"]
+        
     }
 
     g = tracery.createGrammar(data)
